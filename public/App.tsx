22c41e5552e05df12262541b4a3589d9f68d051e
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2025-04-16 15:07:06 +0300

    Update App.tsx

--- public/App.tsx
+++ public/App.tsx
@@ -193,7 +193,7 @@ function App() {
               },
               {
                 title: "Community Service",
-                date: "Annually,
+                date: "Annually",
                 description: "Serving our local community through various projects."
               }
             ].map((event, index) => (
